2ba0596870ad4303997ac4557209eda1503c08b8
Therushmorare <tshego.tt.morare@gmail.com>
2024-03-21 17:52:51 -0700

    task 3 fix

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,23 +1,4 @@
--- Create a temporary table to hold the lifespan in years
-CREATE TEMPORARY TABLE IF NOT EXISTS temp_bands AS
-SELECT
-    band_name,
-    FLOOR(SPLIT(born, '-', 1)) AS formed,
-    CASE
-        WHEN died = 'present' THEN 2022 - FLOOR(SPLIT(born, '-', 1))
-        ELSE FLOOR(SPLIT(died, '-', 1)) - FLOOR(SPLIT(born, '-', 1))
-    END AS lifespan
-FROM
-    bands;
-
--- Select bands with Glam rock as their main style and rank them by longevity
-SELECT
-    band_name,
-    lifespan
-FROM
-    temp_bands
-WHERE
-    band_name IN (SELECT band_name FROM styles WHERE style = 'Glam rock')
-ORDER BY
-    lifespan DESC;
+-- Create a temporary table to hold the lifespan
+SELECT band_name, COALESCE(split, 2022) - formed as lifespan FROM metal_bands
+WHERE style LIKE '%Glam rock%' ORDER BY lifespan DESC;
 
