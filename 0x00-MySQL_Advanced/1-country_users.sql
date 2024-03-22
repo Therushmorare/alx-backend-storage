a54da4efd0181fbd5d417344e09a05a7c63cc594
Therushmorare <tshego.tt.morare@gmail.com>
2024-03-21 17:45:33 -0700

    task 1 fix

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,7 +1,8 @@
---Table for users with country
+-- Check if the table already exists
 CREATE TABLE IF NOT EXISTS users (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255),
-    country ENUM('US', 'CO', 'TN') NOT NULL
+    country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
 );
+
